c588e1aea1d4de0a8a0a4296b38c4146d762cc3d
DanielrDGT <daniel.rondinel@dgtalliance.com>
2022-09-16 22:10:30 -0500

    interfaces mainfutbol

--- app/src/main/java/com/upc/polideportivoapp/MainActivity.kt
+++ app/src/main/java/com/upc/polideportivoapp/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.upc.polideportivoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btnIngresoFutbol:ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        btnIngresoFutbol = findViewById(R.id.btnIngresoFutbol)
+        btnIngresoFutbol.setOnClickListener{
+            val intent = Intent(this,MainFutbol::class.java)
+            startActivity(intent)
+        }
        }
+
 }
