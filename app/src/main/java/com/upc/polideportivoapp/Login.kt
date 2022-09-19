56354a19664e65c055514a1d4059ceb61af8fe69
DanielrDGT <daniel.rondinel@dgtalliance.com>
2022-09-18 22:55:53 -0500

    push18-09

--- app/src/main/java/com/upc/polideportivoapp/Login.kt
+++ app/src/main/java/com/upc/polideportivoapp/Login.kt
@@ -1,12 +1,26 @@
 package com.upc.polideportivoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
+
+    private lateinit var btnRegistrar:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        asignarReferencias()
+    }
+
+    private fun asignarReferencias(){
+        btnRegistrar = findViewById(R.id.btnRegistrar)
+        btnRegistrar.setOnClickListener {
+            val intent =  Intent(this,RegistrarLogin::class.java)
+            startActivity(intent)
+        }
 
     }
 }
