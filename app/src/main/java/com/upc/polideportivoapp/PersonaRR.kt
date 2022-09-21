36f99fdaf8e52b1840208a55897aeea2622b4dcb
DanielrDGT <daniel.rondinel@dgtalliance.com>
2022-09-20 21:50:15 -0500

    push20-09/2

--- app/src/main/java/com/upc/polideportivoapp/PersonaRR.kt
+++ app/src/main/java/com/upc/polideportivoapp/PersonaRR.kt
@@ -3,10 +3,10 @@ package com.upc.polideportivoapp
 import android.widget.Spinner
 import java.util.*
 
-class PersonaRR(nombresRR:String, telefonoRR:String, emailRR:String,dtDatePicker1:String, spinner1:String ) {
+class PersonaRR(nombresRR:String, telefonoRR:String, emailRR:String,dtDatePicker1:String,mostrarFecha:String  ) {
     var nombres:String = nombresRR
     var telefono:String = telefonoRR
     var email:String = emailRR
-    var Fecha:String = spinner1
+    var Fecha:String = mostrarFecha
     var Turno:String = dtDatePicker1
 }
--- app/src/main/java/com/upc/polideportivoapp/RegistroReserva.kt
+++ app/src/main/java/com/upc/polideportivoapp/RegistroReserva.kt
@@ -13,6 +13,7 @@ class RegistroReserva : AppCompatActivity() {
 
         private lateinit var dtDatePicker1:EditText
         private lateinit var spinner1:Spinner
+        private lateinit var mostrarFecha:EditText
         private lateinit var txtNombresRR:EditText
         private lateinit var txtTelefonoRR:EditText
         private lateinit var txtEmailRR:EditText
@@ -33,8 +34,8 @@ class RegistroReserva : AppCompatActivity() {
         }
 
         // spinner
-        spinner1 = findViewById(R.id.spinner1)
-        ArrayAdapter.createFromResource(
+      //  spinner1 = findViewById(R.id.spinner1)
+        /*   ArrayAdapter.createFromResource(
             this,R.array.horas,
             android.R.layout.simple_spinner_dropdown_item)
             .also {
@@ -42,6 +43,31 @@ class RegistroReserva : AppCompatActivity() {
                 spinner1.adapter = adapter
             }
 
+        //spinner2
+        var spinner1 = findViewById<Spinner>(R.id.spinner1)
+        var lista = resources.getStringArray(R.array.horas)
+        var adaptador = ArrayAdapter(this,android.R.layout.simple_spinner_item,lista)
+        spinner1.adapter = adaptador
+ */        //spinner3
+        var lista = resources.getStringArray(R.array.horas)
+        spinner1 = findViewById(R.id.spinner1)
+        mostrarFecha = findViewById(R.id.mostrarfecha)
+        val adaptador = ArrayAdapter(this,android.R.layout.simple_spinner_item,lista)
+        spinner1.adapter = adaptador
+        spinner1.onItemSelectedListener = object:
+            AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?,
+                                        p1: View?,
+                                        p2: Int,
+                                        p3: Long) {
+                mostrarFecha.setText(spinner1.selectedItem.toString())
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
     }
     //calendario
     private fun showDatePickerDialog() {
@@ -69,12 +95,14 @@ class RegistroReserva : AppCompatActivity() {
         val telefonoRR = txtTelefonoRR.text.toString()
         val emailRR = txtEmailRR.text.toString()
         val dtDatePicker1= dtDatePicker1.text.toString()
-        val spinner1 = setOf(spinner1).toString()
+        val mostrarFecha = mostrarFecha.text.toString()
 
-        val persona = PersonaRR(nombresRR, telefonoRR,emailRR,spinner1,dtDatePicker1)
-
-        referencia.child(referencia.push().key.toString()).setValue(persona)
-        finish()
-
+        val persona = PersonaRR(nombresRR, telefonoRR,emailRR,mostrarFecha,dtDatePicker1)
+        if(nombresRR.isEmpty() || telefonoRR.isEmpty() || emailRR.isEmpty() || mostrarFecha.isEmpty() || dtDatePicker1.isEmpty()){
+            Toast.makeText(this,"Complete todos los campos", Toast.LENGTH_SHORT).show()
+        }else{
+            referencia.child(referencia.push().key.toString()).setValue(persona)
+            finish()
+        }
     }
 }
